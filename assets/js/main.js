7ab0bf6209c53496a3cc8d60c5fdfdd54108d626
Samira Cohanim <samiracohanim@Samiras-MacBook-Pro.local>
2020-11-17 10:55:46 -0600

    add sprites

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,25 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image('background', 'assets/img/background.png');
   this.load.image('player', 'assets/img/player.png');
+
+  // this.load.spritesheet('kiki-sprite', 'assets/img/kiki-sprite.png', 3, 5);
 }
+
+// const platforms;
 
 function create() {
   this.add.image(400, 300, 'background');
-  this.add.image(400, 300, 'player');
+
+  // platforms = this.physics.add.staticGroup();
+  // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  this.add.image(100, 300, 'player');
+  // this.add.spritesheet(300, 200, 'kiki-sprite');
+  // this.anims.create('run');
+  // this.anims.create('run', 30, true);
+  player = this.physics.add.sprite(100, 450, 'kiki-sprite');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
 }
 
 function update() {}
