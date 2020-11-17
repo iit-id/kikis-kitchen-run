26e7de805fb91c634b5797ceb237ba0090e6dd03
Samira Cohanim <samiracohanim@Samiras-MacBook-Pro.local>
2020-11-17 11:39:29 -0600

    update sprite

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -15,7 +22,10 @@ function preload() {
   this.load.image('background', 'assets/img/background.png');
   this.load.image('player', 'assets/img/player.png');
 
-  // this.load.spritesheet('kiki-sprite', 'assets/img/kiki-sprite.png', 3, 5);
+  this.load.spritesheet('kiki-sprite', 'assets/img/kiki-sprite.png', {
+    frameWidth: 123,
+    frameHeight: 200,
+  });
 }
 
 // const platforms;
@@ -23,14 +33,15 @@ function preload() {
 function create() {
   this.add.image(400, 300, 'background');
 
-  // platforms = this.physics.add.staticGroup();
+  platforms = this.physics.add.staticGroup();
   // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-  this.add.image(100, 300, 'player');
+  //this.add.image(100, 300, 'player');
   // this.add.spritesheet(300, 200, 'kiki-sprite');
   // this.anims.create('run');
   // this.anims.create('run', 30, true);
-  player = this.physics.add.sprite(100, 450, 'kiki-sprite');
+  player = this.physics.add.sprite(100, 50, 'kiki-sprite');
+  player.setScale(1);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 }
