bd6114d160db16179b7ae0e2248f852b03ecd8fa
Samira Cohanim <samiracohanim@Samiras-MacBook-Pro.local>
2020-11-20 16:20:53 -0800

    add keyboard controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,6 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'assets/img/background.png');
-  this.load.image('player', 'assets/img/player.png');
 
   this.load.spritesheet('kiki-sprite', 'assets/img/kiki-sprite.png', {
     frameWidth: 123,
@@ -35,15 +34,52 @@ function create() {
 
   platforms = this.physics.add.staticGroup();
 
-  player = this.physics.add.sprite(100, 50, 'kiki-sprite');
-  player.setScale(1);
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
   platforms.create(400, 568, 'platform').refreshBody();
 
   platforms.create(400, 350, 'platform');
   platforms.create(400, 150, 'platform');
+
+  player = this.physics.add.sprite(100, 50, 'kiki-sprite');
+
+  player.setScale(1);
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  scoreText = this.add.text(16, 16, 'score: 0', {
+    fontSize: '32px',
+    fill: '#000',
+  });
+
+  this.physics.add.collider(player, platforms);
+
+  player.body.setGravityY(300);
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+
+  function collectFood(player, food) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+  }
+}
