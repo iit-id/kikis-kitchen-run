ad953a8e3fe515db5223e8743c645067304d7e5e
Samira Cohanim <samiracohanim@Samiras-MacBook-Pro.local>
2020-11-21 15:51:49 -0800

    add food images

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,22 +58,8 @@ function create() {
 }
 
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+  if ((cursors.space.isDown || cursors.up.isDown) && player.body.onFloor()) {
+    player.body.setVelocityY(-600); // jump up
   }
 
   function collectFood(player, food) {
